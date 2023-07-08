d2c9e7bd29186d7f5d7e0ae35acc16d3fc6f0fee
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2023-07-08 17:51:39 +0100

    Omitted some forgotten console logs, and WIP tailwind closed page

--- express-routing/server.js
+++ express-routing/server.js
@@ -5,13 +5,35 @@ const timeChecker = (req, res, next) => {
     const currentDay = new Date().getDay();
     const currentHour = new Date().getHours();
 
-    console.log(currentHour);
-    console.log(currentDay);
-
     if (currentDay >= 1 && currentDay <= 5 && currentHour >= 9 && currentHour < 17) {
         next();
     } else {
-        res.send('The website is only available during working hours (Monday to Friday, 9h to 17h).');
+        res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="/styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+      </head>
+      <body>
+        <main className="relative isolate min-h-full">
+        <img
+          src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+          alt=""
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">We are closed</h1>
+          <p className="mt-4 text-base text-white/70 sm:mt-6">The website is only available during working hours (Monday to Friday, 9h to 17h).</p>
+          <div className="mt-10 flex justify-center">
+            <a href="/" className="text-sm font-semibold leading-7 text-white">
+              <span aria-hidden="true">&larr;</span> Retry again by going back to home?
+            </a>
+          </div>
+        </div>
+      </main>
+      </body>
+    </html>`);
     }
 }
 
@@ -23,6 +45,7 @@ app.get('/', (req, res) => {
     <html>
       <head>
         <link rel="stylesheet" href="/styles.css">
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>
         <nav>
@@ -42,6 +65,7 @@ app.get('/services', (req, res) => {
     <html>
       <head>
         <link rel="stylesheet" href="/styles.css">
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>
         <nav>
@@ -61,6 +85,7 @@ app.get('/contact', (req, res) => {
     <html>
       <head>
         <link rel="stylesheet" href="/styles.css">
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>
         <nav>
